683b8d4b9e138cd3b53e65dd5ccafc292168233d
bialek <bialekrafal94@gmail.com>
2018-12-18 19:33:20 +0100

    add hero page in progress

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 document.querySelector('.nav__mobile').addEventListener('click', function() {
     const nav = document.querySelector('.nav');
     nav.classList.contains('nav--active') ? nav.classList.remove('nav--active') : nav.classList.add('nav--active');
-})
+    nav.style.width = document.width;
+});
+
