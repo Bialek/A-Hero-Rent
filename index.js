2541139890e21a577da4a28bc7c64200907268b5
bialek <bialekrafal94@gmail.com>
2019-02-13 16:51:59 +0100

    preparing to deploy on heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
-const PORT = 3000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/public'))
-
-app.use(require('cors')())
-app.use(require('body-parser').json())
-app.use(require('./routes'))
-
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(require('cors')())
+    .use(require('body-parser').json())
+    .use(require('./routes'))
+    .get('/', (req, res) => res.render('public/index'))
+    .listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
